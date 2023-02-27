0531b7dde73db1d737099db365e04195d5df7541
Zhong Yi <yi.zhong@student-cs.fr>
2023-02-27 16:12:41 +0100

    complet model

--- mnist_model.py
+++ mnist_model.py
@@ -1,26 +1,143 @@
-import os
 import torch
 from torch import nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+from torchvision.transforms import ToTensor
+from torch import optim
+from torch.autograd import Variable
+import cv2
 
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+            ),
             nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10),
+            nn.MaxPool2d(kernel_size=2),
         )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+        # fully connected layer, output 10 classes
+        self.out = nn.Linear(32 * 7 * 7, 10)
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)
+        output = self.out(x)
+        return output, x
 
 
-model = NeuralNetwork()
+train_data = datasets.MNIST(
+    root='data',
+    train=True,
+    transform=ToTensor(),
+    download=True,
+)
+test_data = datasets.MNIST(
+    root='data',
+    train=False,
+    transform=ToTensor()
+)
+
+loaders = {
+    'train': torch.utils.data.DataLoader(train_data,
+                                         batch_size=100,
+                                         shuffle=True,
+                                         num_workers=1),
+
+    'test': torch.utils.data.DataLoader(test_data,
+                                        batch_size=100,
+                                        shuffle=True,
+                                        num_workers=1),
+}
+
+
+def train(num_epochs, cnn, loaders):
+
+    cnn.train()
+
+    # Train the model
+    total_step = len(loaders['train'])
+
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(loaders['train']):
+
+            # gives batch data, normalize x when iterate train_loader
+            b_x = Variable(images)   # batch x
+            b_y = Variable(labels)   # batch y
+
+            output = model(b_x)[0]
+            loss = loss_func(output, b_y)
+
+            # clear gradients for this training step
+            optimizer.zero_grad()
+
+            # backpropagation, compute gradients
+            loss.backward()
+            # apply gradients
+            optimizer.step()
+
+            if (i+1) % 100 == 0:
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(
+                    epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+            pass
+        pass
+    pass
+
+
+def test():
+    # Test the model
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in loaders['test']:
+            test_output, last_layer = model(images)
+            pred_y = torch.max(test_output, 1)[1].data.squeeze()
+            accuracy = (pred_y == labels).sum().item() / float(labels.size(0))
+            pass
+        print('Test Accuracy of the model on the 10000 test images: %.2f' % accuracy)
+
+    pass
+
+
+def pre_image(img_path, model):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    mean = [0.485]
+    std = [0.229]
+    transform_norm = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Resize((28, 28)), transforms.Normalize(mean, std)])
+    # get normalized image
+    img_normalized = transform_norm(img).float()
+    img_normalized = img_normalized.unsqueeze_(0)
+    # input = Variable(image_tensor)
+    # print(img_normalized.shape)
+    with torch.no_grad():
+        model.eval()
+        output, l = model(img_normalized)
+        pred_y = torch.max(output, 1)[1].data.squeeze()
+        return pred_y
+
+
+if __name__ == '__main__':
+    model = NeuralNetwork()
+    num_epochs = 2
+    loss_func = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    train(num_epochs, model, loaders)
+    test()
+    print(pre_image("data/3 test.png", model))
